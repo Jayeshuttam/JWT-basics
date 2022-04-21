d5cd0c56fadd8af016735c4954dbd44e9666e65e
Jayeshuttam <jayeshuttam7844@gmail.com>
2022-04-21 14:56:27 -0300

    added some functinaltiy in Controller

--- controllers/main.js
+++ controllers/main.js
@@ -18,9 +18,24 @@ const login = async (req,res)=>{
 }
 
 const dashboard = async(req,res)=>{
-    
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ') ){
+        throw new CustomAPIError("No token Provided",401)
+    }
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        console.log(decoded);
+        
+    res.status(200).json({msg:`hello,${decoded.username}` ,secret:`here is your authorized data ${luckyNumber}`})
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route',401)
+    }
+
+
     const luckyNumber =Math.floor(Math.random()*100)
-    res.status(200).json({msg:"hello John doe",secret:`here is your authorized data ${luckyNumber}`})
 }
 
 module.exports = {login,dashboard}
